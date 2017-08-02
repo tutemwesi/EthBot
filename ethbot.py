1d51db148f49148a5aab723f1d4f720e453c6794
tutemwesi <30622268+tutemwesi@users.noreply.github.com>
2017-08-02 12:48:48 +0100

    Update ethbot.py

--- ethbot.py
+++ ethbot.py
@@ -4,77 +4,67 @@ import urllib.request
 from urllib.request import urlopen
 import json
 
-# Headers are required to communicate with APIs 
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"}
-
-#API URLs - be sure to replace <wallet address> with the wallet you are interested in 
-urlnano = "https://api.nanopool.org/v1/eth/user/<wallet address>/"
-urleth = "https://api.kraken.com/0/public/Ticker?pair=ETHGBP"
-urlbtc = "https://api.kraken.com/0/public/Ticker?pair=XBTGBP"
-
-
-#This bit is shit and I'm sure there is a better way of doing it
-
-reqnano = urllib.request.Request(urlnano, headers = headers)
-reqeth = urllib.request.Request(urleth, headers = headers)
-reqbtc = urllib.request.Request(urlbtc, headers = headers)
-
-xeth = urllib.request.urlopen(reqeth)
-xnano = urllib.request.urlopen(reqnano)
-xbtc = urllib.request.urlopen(reqbtc)
-
-datanano = xnano.read()
-dataeth = xeth.read()
-databtc = xbtc.read()
-
-encodingnano = xnano.info().get_content_charset('utf-8')
-encodingeth = xeth.info().get_content_charset('utf-8')
-encodingbtc = xbtc.info().get_content_charset('utf-8')
-
-
-ethprice = json.loads(dataeth.decode(encodingeth))
-btcprice = json.loads(databtc.decode(encodingbtc))
-hashrate = json.loads(datanano.decode(encodingnano))
-
-#Obtain a token from the Discord API and set below  
-
-TOKEN = 'Enter your Discord Token here'
+#Set Discord API bot token here
+TOKEN = ''
 
 description = '''EthBot'''
-bot = commands.Bot(command_prefix='!', description=description)
+bot = commands.Bot(command_prefix='?', description=description)
 
 
 @bot.event
 async def on_ready():
     print('Logged in as')
     print(bot.user.name)
-    print(bot.user.id)  
+    print(bot.user.id)
     print('------')
 
-
-@bot.command()
-async def hello():
-    """Says world"""
-    await bot.say("world")
-
-@bot.command()
-async def hash():
-    """Show Average Hashrate"""
-    await bot.say("Average 6hr Hashrate: " + str(hashrate['data']['avgHashrate']['h6']))
-
+#Set the wallet address under URL for each instance
 @bot.command()
 async def balance():
-    """Show Current Balance"""
-    await bot.say("Current Balance: " + str(hashrate['data']['balance']))
+	"""Show Current Balance"""
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"}
+    url = "https://api.nanopool.org/v1/eth/user/Wallet Address /"
+    req = urllib.request.Request(url, headers = headers)
+    x = urllib.request.urlopen(req)
+    data = x.read()
+    encoding = x.info().get_content_charset('utf-8')
+    balance = json.loads(data.decode(encoding))
+    await bot.say("Current Balance: " + str(balance['data']['balance']))
+
+@bot.command()
+async def hashrate():
+	"""Show Current Hashrate"""
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"}
+    url = "https://api.nanopool.org/v1/eth/user/Wallet Address /"
+    req = urllib.request.Request(url, headers = headers)
+    x = urllib.request.urlopen(req)
+    data = x.read()
+    encoding = x.info().get_content_charset('utf-8')
+    hashrate = json.loads(data.decode(encoding))
+    await bot.say("Current Hashrate: " + str(hashrate['data']['avgHashrate']['h6']))
 
 @bot.command()
 async def eth():
-    """Show Current Eth to GBP Conversion Rate"""
+	"""Show Current Eth to GBP Conversion Rate"""
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"}
+    url = "https://api.kraken.com/0/public/Ticker?pair=ETHGBP"
+    req = urllib.request.Request(url, headers = headers)
+    x = urllib.request.urlopen(req)
+    data = x.read()
+    encoding = x.info().get_content_charset('utf-8')
+    ethprice = json.loads(data.decode(encoding))
     await bot.say("1.0 ETH = £" + str(ethprice['result']['XETHZGBP']['a'][0]))
 
 @bot.command()
 async def btc():
-    """Show Current BTC to GBP Conversion Rate"""
+	"""Show Current BTC to GBP Conversion Rate"""
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"}
+    url = "https://api.kraken.com/0/public/Ticker?pair=ETHGBP"
+    req = urllib.request.Request(url, headers = headers)
+    x = urllib.request.urlopen(req)
+    data = x.read()
+    encoding = x.info().get_content_charset('utf-8')
+    btcprice = json.loads(data.decode(encoding))
     await bot.say("1.0 BTC = £" + str(btcprice['result']['XXBTZGBP']['a'][0]))
 
 
